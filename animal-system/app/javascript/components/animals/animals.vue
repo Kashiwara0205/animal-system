9bb7c22929c8a604acc434009aa13de20b5b3c2c
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2021-05-01 23:41:53 +0900

    pagination機能を実装

--- animal-system/app/javascript/components/animals/animals.vue
+++ animal-system/app/javascript/components/animals/animals.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="base">
-    <h1> {{ title }} </h1>
+    <h1 style="padding-left: 15px;"> {{ title }} </h1>
+    <pagination v-bind:offset.sync="offset" :total="total" @fetchInfo="fetchInfo"> </pagination>
     <list :info="info" :loading="loading"></list>
   </div>
 </template>
@@ -8,18 +9,22 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import List from "../animals/list.vue"
+import Pagination from "../utils/pagination.vue"
 import Animal from "../../model/animal"
 import http from '../../lib/http'
 import notifier from '../../lib/notifier'
 
 @Component({
-  components:{ list: List }
+  components:{ 
+    list: List,
+    pagination: Pagination
+  }
 })
 export default class Animals extends Vue {
   private title = "動物情報 一覧"
   private model = new Animal
   private info = []
-  private count = 0
+  private total = 0
   private offset = 0
   private query = {}
   private loading = false
@@ -36,7 +41,7 @@ export default class Animals extends Vue {
     try{
       const res = await http.get(url, params);
       this.info = res["data"]["info"]
-      this.count = res["data"]["count"]
+      this.total = res["data"]["count"]
     }catch(e){
       notifier.notifyError(this)
     }finally{
---
+++ animal-system/app/javascript/components/utils/pagination.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="animal-system-pagination"> 
+    <p style="padding-left: 10px; margin: 5px;"> {{ total }}件のうち{{offset}}~{{nextOffset}}件を表示 </p>
+    <el-pagination
+      layout="prev, pager, next"
+      @current-change="currentChange"
+      :page-size="50"
+      :current-page.sync="currentPage"
+      :total="total">
+    </el-pagination>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+@Component
+export default class List extends Vue {
+  @Prop({ required: true }) offset
+  @Prop({ required: true }) total
+
+  private currentPage = 1
+
+  get nextOffset(){
+    const nextOffset = this.currentPage * 50
+    if(this.total < nextOffset){ return this.total }
+    return nextOffset
+  }
+
+  currentChange(idx){
+    this.$emit('update:offset', (idx - 1) * 50)
+    this.$emit('fetchInfo')
+  }
+}
+</script>
+
+<style>
+
+.btn-next, .btn-prev, .btn-quickprev, .btn-quicknext,
+.el-pagination .number,
+.el-pagination button:disabled,
+.el-pagination .btn-next {
+  background:transparent !important;
+  font-size: 15px;
+}
+
+.btn-next .el-icon, .btn-prev .el-icon  {
+font-size: 18px !important;
+}
+</style>
