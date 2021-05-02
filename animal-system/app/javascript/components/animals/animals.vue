b62e980ff92f445e83218bbae36d3464c8a9be86
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2021-05-02 17:55:26 +0900

    権限表示を別コンポーネント化

--- animal-system/app/javascript/components/animals/animals.vue
+++ animal-system/app/javascript/components/animals/animals.vue
@@ -4,30 +4,21 @@
       <h1 style="padding-left: 15px;"> <i class="el-icon-document"></i> {{ title }} </h1>
 
       <el-tabs type="border-card">
-      <el-tab-pane>
-        <span slot="label"><i class="el-icon-document"></i> 一覧表示</span>
-        <pagination v-bind:offset.sync="offset" :total="total" @fetchInfo="fetchInfo" disp-message> </pagination>
-        <list :info="info" 
-              :loading="loading" 
-              :animal-type-list="animalTypeList" 
-              :countory-list="countoryList" >
-        </list>
-      </el-tab-pane>
-      <el-tab-pane class="panel">  
-        <span slot="label"><i class="el-icon-warning"></i> 権限情報</span>
-        <b> 種類更新機能  </b> <b style="color:red"> [ 操作可能 ] </b>
-        <li> 部署「事務」に所属する者は可能 </li>
-        <li> 職位「リーダー」以上の権限を持つ者は可能 </li>
-
-        <br>
-        <b> 原産国更新機能 </b> <b style="color:red"> [ 操作可能 ] </b>
-        <li> 部署「事務」に所属する者は可能 </li>
-        <li> 職位「リーダー」以上の権限を持つ者は可能 </li>
-        <br>
-        <b> 体毛更新機能 </b>
-        <li> ○○さんのみ可能 </li>
-      </el-tab-pane>
-    </el-tabs>
+        <el-tab-pane>
+          <span slot="label"><i class="el-icon-document"></i> 一覧表示</span>
+          <pagination v-bind:offset.sync="offset" :total="total" @fetchInfo="fetchInfo" disp-message> </pagination>
+          <list :info="info" 
+                :loading="loading" 
+                :animal-type-list="animalTypeList" 
+                :countory-list="countoryList" >
+          </list>
+        </el-tab-pane>
+        <el-tab-pane class="panel">
+          <span slot="label"><i class="el-icon-warning"></i> 権限表示</span>
+          <authentication-list></authentication-list>
+        </el-tab-pane>
+      </el-tabs>
+      
     </div>
   </div>
 </template>
@@ -35,6 +26,7 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import List from "../animals/list.vue"
+import AuthenticationList from "../animals/authentication_list.vue"
 import Pagination from "../utils/pagination.vue"
 import Animal from "../../model/animal"
 import AnimalType from "../../model/animal_type"
@@ -46,6 +38,7 @@ import utils from "../../lib/utils"
 @Component({
   components:{ 
     list: List,
+    "authentication-list": AuthenticationList,
     pagination: Pagination
   }
 })
---
+++ animal-system/app/javascript/components/animals/authentication_list.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="authentication-list">
+    <p> ※ 現在割り当てられている権限を表示しています </p>
+    <el-table
+      :data="info"
+      height="500"
+      border
+      style="width: 100%;">
+      <el-table-column label="機能名" prop="name"></el-table-column>
+      <el-table-column label="操作可/不可" prop="permission"></el-table-column>
+      <el-table-column label="説明" prop="description"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+@Component
+export default class List extends Vue {
+  private info = [
+    { name: "更新機能（一覧）", permission: "操作可", description: "職位「リーダー」の者のみ操作可能" },
+    { name: "検索機能", permission: "操作可", description: "全ての者が操作可能" }
+  ]
+}
+</script>
+
+<style scoped>
+</style>
