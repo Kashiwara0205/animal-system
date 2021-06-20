09c1bef751e2526e64635600225efc8df34f78c0
kashiwara <tanakatarou0205@gmail.com>
2021-06-20 13:28:09 +0900

    recommendation-systemの土台となるアプリケーションを追加

---
+++ recommendation-system/code/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# 類似性の高い質問を返す
+# 以下の２つのフィルタリングから構成
+#
+#  内容ベースの類似質問
+# - 類似質問の抽出 => 質問内容とタイトルをベースにしたコンテンツベースのフィルタリング
+#   「似たような質問は、こちらです」といった形式で出力できる
+#
+#  ユーザ属性ごとの類似質問
+# - 類似ユーザが作成した質問内容の抽出 => ユーザの登録情報をベースにした協調ベースのフィルタリング
+#   「犬を飼っている人は、他にもこんな質問をしました」といった形式で出力できる
+#
+# example: curl localhost:5555/api/v1/similarity_question/3
+@app.route('/api/v1/similarity_question/<int:id>', methods=['GET'])
+def get_similarity_question(id):
+  return 'User %d' % id
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5555, debug=True)
