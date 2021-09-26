c906124b401240557c56dad335f835be10e3720c
kashiwara <tanakatarou0205@gmail.com>
2021-09-26 12:23:54 +0900

    downlad_btnコンポーネントのスタイルを整えた

--- animal-system/app/javascript/components/animals/list_panel/download_btn.vue
+++ animal-system/app/javascript/components/animals/list_panel/download_btn.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-button type="success" size="mini" @click="onDownload"class="download-btn">
+    <el-button type="success" size="mini" @click="onDownload" class="download-btn">
       CSVダウンロード <i class="el-icon-download"></i>
     </el-button>
   </div>
